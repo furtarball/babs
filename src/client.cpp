db6e76b770741035f5c330071ca9fb544bc6f9f0
furtarball <61846505+furtarball@users.noreply.github.com>
2025-01-02 22:50:55 +0100

    Allow for arbitrary server choice and arbitrary message contents (revolutionary)

--- src/client.cpp
+++ src/client.cpp
@@ -1,58 +1,64 @@
-#include <array>
-#include <iostream>
-#include <boost/asio.hpp>
-#include <sstream>
-#include <cstdint>
 #include "packet.h"
 #include "session.h"
+#include <array>
+#include <boost/asio.hpp>
+#include <cstdint>
+#include <iostream>
+#include <sstream>
 #include <string>
 
 using boost::asio::ip::tcp;
 
-class Client {
-  
-};
+class Client {};
 
 int main(int argc, char* argv[]) {
-  boost::system::error_code error;
-  try {
+	boost::system::error_code error;
+	try {
 
-    boost::asio::io_context io_context;
+		boost::asio::io_context io_context;
 
-    tcp::resolver resolver(io_context);
-    tcp::resolver::results_type endpoints =
-      resolver.resolve("10.1.0.2", "52137", error);
-    if(error) throw boost::system::system_error(error);
+		tcp::resolver resolver(io_context);
+		std::cout << "łączenie z " << argv[1] << std::endl;
+		tcp::resolver::results_type endpoints =
+			resolver.resolve(argv[1], "52137", error);
+		if (error)
+			throw boost::system::system_error(error);
 
-    tcp::socket socket(io_context);
-    boost::asio::connect(socket, endpoints, error);
-    if(error) throw boost::system::system_error(error);
+		tcp::socket socket(io_context);
+		boost::asio::connect(socket, endpoints, error);
+		if (error)
+			throw boost::system::system_error(error);
+		HelloPacket p;
+		std::cout << "czekam na hello pakiet" << std::endl;
+		receive(socket, p, error);
+		if (error)
+			throw boost::system::system_error(error);
+		std::cout << p.name << std::endl;
 
-    for(;;) {
-      HelloPacket p;
-      std::cout << "czekam na hello pakiet" << std::endl;
-      receive(socket, p, error);
-      if(error == boost::asio::error::eof) break;
-      else if (error) throw boost::system::system_error(error);
-      std::cout << p.name << std::endl;
+		user_id_t uid = std::stoi(std::string(argv[2]));
+		LoginPacket l(0, uid);
+		prepare(l);
+		send(socket, l, error);
+		if (error)
+			throw boost::system::system_error(error);
 
-      user_id_t uid = std::stoi(std::string(argv[1]));
-      LoginPacket l(0, uid);
-      prepare(l);
-      send(socket, l, error);
+		for (;;) {
+			user_id_t recipient;
+			std::cin >> recipient;
+			std::string content;
+			std::getline(std::cin, content);
+			MessagePacket m(0, uid, recipient, content);
+			prepare(m);
+			send(socket, m, error);
 
-      int n;
-      std::cin >> n;
-      MessagePacket m(0, 2137, 666, "okrutniku");
-      prepare(m);
-      send(socket, m, error);
-
-      if(error) throw boost::system::system_error(error);
-    }
-  }
-  catch(std::exception& e) {
-    std::cerr << e.what() << std::endl;
-  }
-
-  return 0;
+			if (error) {
+				if (error == boost::asio::error::eof)
+					break;
+				else
+					throw boost::system::system_error(error);
+			}
+		}
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,16 +1,15 @@
+#include "packet.h"
+#include "session.h"
+#include <boost/asio.hpp>
+#include <cstdint>
 #include <ctime>
 #include <functional>
 #include <iostream>
 #include <memory>
+#include <sstream>
 #include <string>
-#include <boost/asio.hpp>
-#include <iostream>
 #include <unistd.h>
 #include <vector>
-#include <cstdint>
-#include <sstream>
-#include "packet.h"
-#include "session.h"
 
 using boost::asio::ip::tcp;
 const std::uint16_t api_ver = 0;
@@ -18,72 +17,76 @@ const char* name = "Babs Server";
 const std::uint16_t app_ver = 0;
 
 std::string name_version() {
-  return std::string(name) + " v" + std::to_string(api_ver) + "." + std::to_string(app_ver);
+	return std::string(name) + " v" + std::to_string(api_ver) + "." +
+		   std::to_string(app_ver);
 }
 
 class Server {
-  boost::asio::io_context& io_context_;
-  tcp::acceptor acceptor_;
-  std::vector<Session::pointer> sessions;
-  void start_accept() {
-    Session::pointer new_connection = Session::create(io_context_);
-    sessions.push_back(new_connection);
-    acceptor_.async_accept(new_connection->get_socket(),
-			   [this, new_connection](const boost::system::error_code& error) {
-			     if(!error) {
-			       init(new_connection);
-			       recv(new_connection);
-			     }
-			     else throw boost::system::system_error(error);
-			     start_accept();
-			   });
-  }
-public:
-  Server(boost::asio::io_context& io_context)
-    : io_context_(io_context), acceptor_(io_context, tcp::endpoint(tcp::v4(), 52137)) {
-    start_accept();
-  }
-  void init(std::shared_ptr<Session> s) {
-    HelloPacket p(api_ver, name_version());
-    s->async_send(p);
-  }
-  void handle_recv(std::shared_ptr<Packet> pkt, std::shared_ptr<Session> s) {
-    *pkt = Packet(pkt->serialized);
-    switch(pkt->preamble.type) {
-    case LOGIN: {
-      LoginPacket lp(pkt->serialized);
-      std::cout << "Zalogowany użytkownik UID = " << lp.uid << std::endl;
-      s->uid = lp.uid;
-      break;
-    }
-    case MESSAGE: {
-      MessagePacket mp(pkt->serialized);
-      std::cout << "Wiadomość od " << mp.from_uid << " do " << mp.to_uid << ": „" << mp.content << "”" << std::endl;
-      for(auto&& i : sessions) {
-	if(i->uid == mp.to_uid) {
-	  std::cout << "wysyłanie…" << std::endl;
-	  i->async_send(mp);
+	boost::asio::io_context& io_context_;
+	tcp::acceptor acceptor_;
+	std::vector<Session::pointer> sessions;
+	void start_accept() {
+		Session::pointer new_connection = Session::create(io_context_);
+		sessions.push_back(new_connection);
+		acceptor_.async_accept(
+			new_connection->get_socket(),
+			[this, new_connection](const boost::system::error_code& error) {
+				if (!error) {
+					init(new_connection);
+					recv(new_connection);
+				} else
+					throw boost::system::system_error(error);
+				start_accept();
+			});
 	}
-      }
-      break;
-    }
-    }
-  }
-  void recv(std::shared_ptr<Session> s) {
-    s->async_receive([this, s](std::shared_ptr<Packet> p){
-      this->handle_recv(p, s);
-      this->recv(s);
-    });
-  }
+
+	public:
+	Server(boost::asio::io_context& io_context)
+		: io_context_(io_context),
+		  acceptor_(io_context, tcp::endpoint(tcp::v4(), 52137)) {
+		start_accept();
+	}
+	void init(std::shared_ptr<Session> s) {
+		HelloPacket p(api_ver, name_version());
+		s->async_send(p);
+	}
+	void handle_recv(std::shared_ptr<Packet> pkt, std::shared_ptr<Session> s) {
+		*pkt = Packet(pkt->serialized);
+		switch (pkt->preamble.type) {
+		case LOGIN: {
+			LoginPacket lp(pkt->serialized);
+			std::cout << "Zalogowany użytkownik UID = " << lp.uid << std::endl;
+			s->uid = lp.uid;
+			break;
+		}
+		case MESSAGE: {
+			MessagePacket mp(pkt->serialized);
+			std::cout << "Wiadomość od " << mp.from_uid << " do " << mp.to_uid
+					  << ": „" << mp.content << "”" << std::endl;
+			for (auto&& i : sessions) {
+				if (i->uid == mp.to_uid) {
+					std::cout << "wysyłanie…" << std::endl;
+					i->async_send(mp);
+				}
+			}
+			break;
+		}
+		}
+	}
+	void recv(std::shared_ptr<Session> s) {
+		s->async_receive([this, s](std::shared_ptr<Packet> p) {
+			this->handle_recv(p, s);
+			this->recv(s);
+		});
+	}
 };
 
 int main() {
-  try {
-    boost::asio::io_context io_context;
-    Server server(io_context);
-    io_context.run();
-  }
-  catch(std::exception& e) {
-    std::cerr << e.what() << std::endl;
-  }
+	try {
+		boost::asio::io_context io_context;
+		Server server(io_context);
+		io_context.run();
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
 }
